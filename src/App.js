757bf61ab85599b78f096de88ad688f497757a87
SumaiyaK05 <siyanakhandakar@gmail.com>
2022-10-13 03:30:00 +1100

    Reset funtionality added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import ResetPassword from './components/ResetPassword';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ResetPassword />
+    </div>
+  );
+}
+
+
+
+
+export default App;
